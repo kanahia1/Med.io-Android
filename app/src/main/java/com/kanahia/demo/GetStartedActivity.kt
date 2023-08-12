f7a58aca4730207c802c1da63b7e147381ffba24
Kanahia <kaushalkanahia@gmail.com>
2023-08-13 02:43:19 +0530

    Created get started screen

--- app/src/main/java/com/kanahia/demo/GetStartedActivity.kt
+++ app/src/main/java/com/kanahia/demo/GetStartedActivity.kt
@@ -3,25 +3,35 @@ package com.kanahia.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.kanahia.demo.ViewModel.MainViewModel
 import com.kanahia.demo.ViewModel.MainViewModelFactory
 import com.kanahia.demo.api.ApiRepository
 import com.kanahia.demo.api.ApiService
 import com.kanahia.demo.api.RetrofitHelper
+import com.kanahia.demo.databinding.ActivityGetStartedBinding
+import com.realpacific.clickshrinkeffect.applyClickShrink
 
 class GetStartedActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
+    private lateinit var b : ActivityGetStartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        b = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        getSupportActionBar()?.hide()
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+        b.signinBtn.applyClickShrink()
 
         val apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
         val repository = ApiRepository(apiService)
         mainViewModel = ViewModelProvider(this,
             MainViewModelFactory(repository)
         ).get(MainViewModel::class.java)
-
 
         mainViewModel.data.observe(this){
             Log.e("PRINT",it.toString())
--- app/src/main/java/com/kanahia/demo/MainActivity.kt
+++ app/src/main/java/com/kanahia/demo/MainActivity.kt
@@ -46,25 +46,15 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
-        // on below line we are calling
-        // handler to run a task
-        // for specific time interval
+        getSupportActionBar()?.hide()
         Handler().postDelayed({
-            // on below line we are
-            // creating a new intent
             val i = Intent(
                 this@MainActivity,
                 GetStartedActivity::class.java
             )
-            // on below line we are
-            // starting a new activity.
             startActivity(i)
-
-            // on the below line we are finishing
-            // our current activity.
             finish()
-        }, 2000)
+        }, 3000)
 //        binding.loginBtn.setOnClickListener {
 //
 //            WebAuthProvider.login(account)
