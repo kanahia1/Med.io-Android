a0e0be012d32760fb67c37a821ee86c607526c42
Kanahia <kaushalkanahia@gmail.com>
2023-08-20 19:57:37 +0530

    Sign In activity completed

--- app/src/main/java/com/kanahia/demo/utils/BootstrapEditText.kt
+++ app/src/main/java/com/kanahia/demo/utils/BootstrapEditText.kt
@@ -1,17 +1,41 @@
 package com.kanahia.demo.utils
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.kanahia.demo.R
 
 
 class BootstrapEditText(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
     var editText : EditText? = null
+    var background : LinearLayout? = null
+    var fore : LinearLayout? = null
 
     init {
-        inflate(context, R.layout.bootstrap_edit_text_layout, this);
-       editText = findViewById(R.id.editText)
+        inflate(context, R.layout.bootstrap_edit_text_layout, this)
+        editText = findViewById(R.id.editText)
+        background = findViewById(R.id.bootstrapEditTextBack)
+        fore = findViewById(R.id.bootstrapEditTextFore)
+
+        editText!!.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus){
+                background!!.background = context?.let { ContextCompat.getDrawable(it,R.drawable.bootstrap_edit_text_selected_background) }
+                fore!!.background = context?.let { ContextCompat.getDrawable(it,R.drawable.bootstrap_edit_text_selected) }
+            }else{
+                background!!.background = context?.let { ContextCompat.getDrawable(it,R.drawable.bootstrap_edit_text_not_selected_background) }
+                fore!!.background = context?.let { ContextCompat.getDrawable(it,R.drawable.bootstrap_edit_text_not_selected) }
+            }
+        }
+    }
+
+
+
+
+
+    fun getText(): String {
+        return editText!!.text.toString()
     }
 
 
