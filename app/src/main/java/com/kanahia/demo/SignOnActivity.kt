42589764d606f6b977124bd2cc5601fd23202125
Kanahia <kaushalkanahia@gmail.com>
2023-08-20 19:21:18 +0530

    Sign In activity updated

--- app/src/main/java/com/kanahia/demo/SignOnActivity.kt
+++ app/src/main/java/com/kanahia/demo/SignOnActivity.kt
@@ -2,10 +2,15 @@ package com.kanahia.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignOnActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_on)
+
+        getSupportActionBar()?.hide()
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
     }
 }
---
+++ app/src/main/java/com/kanahia/demo/utils/CustomButton.kt
@@ -0,0 +1,18 @@
+package com.kanahia.demo.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+import android.widget.LinearLayout
+import com.kanahia.demo.R
+
+class CustomButton(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+
+
+    init {
+        inflate(context, R.layout.custom_button, this);
+
+    }
+
+
+}
