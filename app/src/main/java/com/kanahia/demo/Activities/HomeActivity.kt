bc614073d23a39584f0b16a94e35831f57933f3e
Kanahia <kaushalkanahia@gmail.com>
2023-08-22 19:06:17 +0530

    Added fragments

--- app/src/main/java/com/kanahia/demo/Activities/HomeActivity.kt
+++ app/src/main/java/com/kanahia/demo/Activities/HomeActivity.kt
@@ -2,8 +2,13 @@ package com.kanahia.demo.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import android.view.View
 import android.widget.Toast
+import com.kanahia.demo.Fragments.CalendarFragment
+import com.kanahia.demo.Fragments.Chat
+import com.kanahia.demo.Fragments.HomeFragment
+import com.kanahia.demo.Fragments.ProfileFragment
 import com.kanahia.demo.R
 import com.kanahia.demo.bottomify.BottomifyNavigationView
 import com.kanahia.demo.bottomify.OnNavigationItemChangeListener
@@ -11,6 +16,13 @@ import com.kanahia.demo.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    val intialFragment = 2 // Just for dev purposes
+
+    val chatFragment = Chat()
+    val homeFragment = HomeFragment()
+    val calendarFragment = CalendarFragment()
+    val profileFragment = ProfileFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -19,15 +31,51 @@ class HomeActivity : AppCompatActivity() {
 
         getSupportActionBar()?.hide()
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+
+
+
+        if (savedInstanceState == null) {
+            changeFragment(intialFragment)
+        }
+
         val bottomify = findViewById<BottomifyNavigationView>(R.id.bottomify_nav)
         bottomify.setOnNavigationItemChangedListener(object : OnNavigationItemChangeListener {
             override fun onNavigationItemChanged(navigationItem: BottomifyNavigationView.NavigationItem) {
-                Toast.makeText(this@HomeActivity,
-                    "Selected item at index ${navigationItem.position}",
-                    Toast.LENGTH_SHORT).show()
+                changeFragment(navigationItem.position)
             }
         })
 
 
     }
+
+    fun changeFragment(index : Int ){
+
+        when(index){
+            0 ->  supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container_view,homeFragment)
+                commit()
+            }
+            1 ->  supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container_view,calendarFragment)
+                commit()
+            }
+            2 ->  supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container_view,chatFragment)
+                commit()
+            }
+            3 -> supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container_view,profileFragment)
+                commit()
+            }
+            else ->  supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container_view,homeFragment)
+                commit()
+            }
+
+        }
+
+
+    }
+
 }
---
+++ app/src/main/java/com/kanahia/demo/Fragments/Chat.kt
@@ -0,0 +1,6 @@
+package com.kanahia.demo.Fragments
+
+import androidx.fragment.app.Fragment
+import com.kanahia.demo.R
+
+class Chat : Fragment(R.layout.fragment_chat)
